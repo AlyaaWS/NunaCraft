ca6d049d4bc8caceb011e5b3860af4663d403a4e
AlyaaWS <alyawahyuning93@gmail.com>
2025-01-29 00:22:14 +0700

    first commit

---
+++ About/PWL_2595/hello_2595.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program PHP Pertamaku</title>
+</head>
+<body>
+    <?php
+    //ini adalah komentar satu baris
+    # ini komentar satu baris juga
+    /* ini adalah
+       komentar 
+       lebih dari satu baris
+    */
+    echo "Hello, Ini adalah Program PHP Pertamaku!";
+    ?>
+</body>
+</html>
